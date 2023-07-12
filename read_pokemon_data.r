96882eb50d248bd8f347a732dba6dcde740512f2
Dylan White <jdylanwhite5@gmail.com>
2023-07-11 21:50:36 -0400

    started building table for individual mons

--- read_pokemon_data.r
+++ read_pokemon_data.r
@@ -8,9 +8,26 @@ url <- "https://pokemondb.net/pokedex/gengar"
 # Read the URL
 read_html(url)
 
-# Read the body frotabm the page
+# Read the body from the page
 body <- url %>% read_html() %>% html_nodes("body")
 
 # Get the table with the "type" information
 vitalsTables <- body %>% html_nodes("table.vitals-table")
-typeTable <- html_table(vitalsTables[1])
+mainTable <- html_table(vitalsTables[1])[[1]]
+
+# Get the pokemon types from the table
+types <- mainTable[mainTable$X1 == "Type",2] %>% pull %>% strsplit(" ")
+types <- types[[1]]
+
+# Get the height and weight from the table
+height <- typeTable[mainTable$X1 == "Height",2] %>% pull
+weight <- typeTable[mainTable$X1 == "Weight",2] %>% pull
+
+# Put together the main data table for the pokemon
+columnNames <- c("Weight","Height", paste("Type", c(1:length(types))))
+dataTbl <- c(weight,height,types) %>% t %>% as_tibble
+names(dataTbl) <- columnNames
+
+# TODO: Build stats columns
+
+# TODO: Build evolution columns
